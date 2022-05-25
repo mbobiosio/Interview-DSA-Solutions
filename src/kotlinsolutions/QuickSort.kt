485ac2c16eee5af98aa3dcda7a2c5b4f1dd133bc
mbobiosio <cazewonder@gmail.com>
2022-05-25 19:49:00 +0100

    QuickSort.kt

---
+++ src/kotlinsolutions/QuickSort.kt
@@ -0,0 +1,32 @@
+package kotlinsolutions
+
+/**
+ * @Author Mbuodile Obiosio
+ * https://linktr.ee/mbobiosio
+ */
+fun quicksort(items: List<Int>): List<Int> {
+    if (items.count() < 2) {
+        return items
+    }
+    val pivot = items[items.count() / 2]
+
+    val equal = items.filter { it == pivot }
+    println("pivot value is : $equal")
+
+    val less = items.filter { it < pivot }
+    println("Lesser values than pivot : $less")
+
+    val greater = items.filter { it > pivot }
+    println("Greater values than pivot : $greater")
+
+    return quicksort(less) + equal + quicksort(greater)
+}
+
+fun main() {
+    println("Original list:")
+    val numbers = listOf(2, 4, 7, 3, 6, 9, 5, 1, 0)
+    println(numbers)
+    println("Ordered list:")
+    val ordered = quicksort(numbers)
+    println(ordered)
+}
